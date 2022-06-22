409d8c48c6d440c344e0a3c8c623cece5dc3f03e
ihourglass <inversionhourglass@outlook.com>
2022-06-22 20:56:51 +0800

    testing code

---
+++ test/TestAssemblies/BasicUsage/Attributes/LogAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Rougamo.Context;
+using System;
+
+namespace BasicUsage.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class LogAttribute : Rougamo.MoAttribute
+    {
+        public override void OnEntry(MethodContext context)
+        {
+
+        }
+        public override void OnException(MethodContext context)
+        {
+
+        }
+        public override void OnExit(MethodContext context)
+        {
+
+        }
+        public override void OnSuccess(MethodContext context)
+        {
+
+        }
+    }
+}
