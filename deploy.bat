a39e0b515feb7a8e3ee82e13ae21cabd9eab0d26
ihourglass <inversionhourglass@outlook.com>
2023-03-11 14:35:07 +0800

    Remove deploy.bat, use CI instead.

--- deploy.bat
+++
@@ -1,7 +0,0 @@
-@rd /s /q nugets
-call dotnet build Rougamo.sln --configuration Release
-for %%f in (nugets/*.nupkg) do (
-	call dotnet nuget push nugets/%%f -k %IH_API_KEY% -s https://api.nuget.org/v3/index.json
-)
-
-pause
