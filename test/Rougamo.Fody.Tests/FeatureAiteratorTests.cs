10781b2c402657ace2c21329bb1629a8e0300cd6
ihourglass <inversionhourglass@outlook.com>
2023-04-05 16:13:05 +0800

    Test cases for async iterator supports on-demand weaving into code.

---
+++ test/Rougamo.Fody.Tests/FeatureAiteratorTests.cs
@@ -0,0 +1,173 @@
+﻿using BasicUsage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Rougamo.Fody.Tests
+{
+    [Collection(nameof(BasicUsage))]
+    public class FeatureAiteratorTests : TestBase
+    {
+        public FeatureAiteratorTests() : base("BasicUsage.dll")
+        {
+        }
+
+        protected override string RootNamespace => "BasicUsage";
+
+        [Fact]
+        public async Task AiteratorFeatureOnEntryTest()
+        {
+            var instance = GetInstance("FeatureAiteratorUseCase");
+            var actual = (List<string>)instance.Recording;
+
+            actual.Clear();
+            var p1 = nameof(AiteratorFeatureOnEntryTest);
+            var v1 = await ((IAsyncEnumerable<string>)instance.OnEntry(p1)).ToArrayAsync();
+            Assert.Equal(FeatureAiteratorUseCase.Expect_OnEntry, actual);
+            Assert.Equal(p1, v1.Last());
+
+            actual.Clear();
+            var guid1 = Guid.NewGuid();
+            var v2 = await ((IAsyncEnumerable<string>)instance.OnEntry_RewriteArgs(guid1)).ToArrayAsync();
+            Assert.Equal(FeatureAiteratorUseCase.Expect_OnEntry, actual);
+            Assert.Equal(guid1.ToString(), v2.Last());
+
+            actual.Clear();
+            await Assert.ThrowsAsync<NotImplementedException>(async () => { await ((IAsyncEnumerable<string>)instance.OnEntry_Exception()).ToArrayAsync(); });
+            Assert.Equal(FeatureAiteratorUseCase.Expect_OnEntry, actual);
+        }
+
+        [Fact]
+        public async Task AiteratorFeatureOnExceptionTest()
+        {
+            var instance = GetInstance("FeatureAiteratorUseCase");
+            var actual = (List<string>)instance.Recording;
+
+            actual.Clear();
+            var p1 = nameof(AiteratorFeatureOnEntryTest);
+            var v1 = await ((IAsyncEnumerable<string>)instance.OnException(p1)).ToArrayAsync();
+            Assert.Empty(actual);
+            Assert.Equal(p1, v1.Last());
+
+            actual.Clear();
+            var guid1 = Guid.NewGuid();
+            var v2 = await ((IAsyncEnumerable<string>)instance.OnException_RewriteArgs(guid1)).ToArrayAsync();
+            Assert.Empty(actual);
+            Assert.Equal(guid1.ToString(), v2.Last());
+
+            actual.Clear();
+            await Assert.ThrowsAsync<NotImplementedException>(async () => { await ((IAsyncEnumerable<string>)instance.OnException_Exception()).ToArrayAsync(); });
+            Assert.Equal(FeatureAiteratorUseCase.Expect_OnException, actual);
+        }
+
+        [Fact]
+        public async Task AiteratorFeatureOnSuccessTest()
+        {
+            var instance = GetInstance("FeatureAiteratorUseCase");
+            var actual = (List<string>)instance.Recording;
+
+            actual.Clear();
+            var p1 = nameof(AiteratorFeatureOnEntryTest);
+            var v1 = await ((IAsyncEnumerable<string>)instance.OnSuccess(p1)).ToArrayAsync();
+            Assert.Equal(FeatureAiteratorUseCase.Expect_OnSuccess, actual);
+            Assert.Equal(p1, v1.Last());
+
+            actual.Clear();
+            var guid1 = Guid.NewGuid();
+            var v2 = await ((IAsyncEnumerable<string>)instance.OnSuccess_RewriteArgs(guid1)).ToArrayAsync();
+            Assert.Equal(FeatureAiteratorUseCase.Expect_OnSuccess, actual);
+            Assert.Equal(guid1.ToString(), v2.Last());
+
+            actual.Clear();
+            await Assert.ThrowsAsync<NotImplementedException>(async () => { await ((IAsyncEnumerable<string>)instance.OnSuccess_Exception()).ToArrayAsync(); });
+            Assert.Empty(actual);
+        }
+
+        [Fact]
+        public async Task AiteratorFeatureOnExitTest()
+        {
+            var instance = GetInstance("FeatureAiteratorUseCase");
+            var actual = (List<string>)instance.Recording;
+
+            actual.Clear();
+            var p1 = nameof(AiteratorFeatureOnEntryTest);
+            var v1 = await ((IAsyncEnumerable<string>)instance.OnExit(p1)).ToArrayAsync();
+            Assert.Equal(FeatureAiteratorUseCase.Expect_OnExit, actual);
+            Assert.Equal(p1, v1.Last());
+
+            actual.Clear();
+            var guid1 = Guid.NewGuid();
+            var v2 = await ((IAsyncEnumerable<string>)instance.OnExit_RewriteArgs(guid1)).ToArrayAsync();
+            Assert.Equal(FeatureAiteratorUseCase.Expect_OnExit, actual);
+            Assert.Equal(guid1.ToString(), v2.Last());
+
+            actual.Clear();
+            await Assert.ThrowsAsync<NotImplementedException>(async () => { await ((IAsyncEnumerable<string>)instance.OnExit_Exception()).ToArrayAsync(); });
+            Assert.Equal(FeatureAiteratorUseCase.Expect_OnExit, actual);
+        }
+
+        [Fact]
+        public async Task AiteratorFeatureRewriteArgsTest()
+        {
+            var instance = GetInstance("FeatureAiteratorUseCase");
+            var actual = (List<string>)instance.Recording;
+
+            actual.Clear();
+            var p1 = nameof(AiteratorFeatureOnEntryTest);
+            var v1 = await ((IAsyncEnumerable<string>)instance.RewriteArgs(p1)).ToArrayAsync();
+            Assert.Equal(FeatureAiteratorUseCase.Expect_RewriteArgs, actual);
+            Assert.Equal(p1, v1.Last());
+
+            actual.Clear();
+            var guid1 = Guid.NewGuid();
+            var v2 = await ((IAsyncEnumerable<string>)instance.RewriteArgs_RewriteArgs(guid1)).ToArrayAsync();
+            Assert.Equal(FeatureAiteratorUseCase.Expect_RewriteArgs, actual);
+            Assert.NotEqual(guid1.ToString(), v2.Last());
+
+            actual.Clear();
+            await Assert.ThrowsAsync<NotImplementedException>(async () => { await ((IAsyncEnumerable<string>)instance.RewriteArgs_Exception()).ToArrayAsync(); });
+            Assert.Equal(FeatureAiteratorUseCase.Expect_RewriteArgs, actual);
+        }
+
+        [Fact]
+        public async Task AiteratorFeatureOnEntryExitTest()
+        {
+            var instance = GetInstance("FeatureAiteratorUseCase");
+            var actual = (List<string>)instance.Recording;
+
+            actual.Clear();
+            var p1 = nameof(AiteratorFeatureOnEntryTest);
+            var v1 = await ((IAsyncEnumerable<string>)instance.OnEntryExit(p1)).ToArrayAsync();
+            Assert.Equal(FeatureAiteratorUseCase.Expect_OnEntryExit, actual);
+            Assert.Equal(p1, v1.Last());
+
+            actual.Clear();
+            var guid1 = Guid.NewGuid();
+            var v2 = await ((IAsyncEnumerable<string>)instance.OnEntryExit_RewriteArgs(guid1)).ToArrayAsync();
+            Assert.Equal(FeatureAiteratorUseCase.Expect_OnEntryExit, actual);
+            Assert.Equal(guid1.ToString(), v2.Last());
+            actual.Clear();
+
+            actual.Clear();
+            await Assert.ThrowsAsync<NotImplementedException>(async () => { await ((IAsyncEnumerable<string>)instance.OnEntryExit_Exception()).ToArrayAsync(); });
+            Assert.Equal(FeatureAiteratorUseCase.Expect_OnEntryExit, actual);
+        }
+
+        [Fact]
+        public async Task DifferentFeatureApplyTest()
+        {
+            var instance = GetInstance("FeatureAiteratorUseCase");
+            var actual = (List<string>)instance.Recording;
+
+            actual.Clear();
+            await Assert.ThrowsAsync<NotImplementedException>(async () => await ((IAsyncEnumerable<string>)instance.DiffApplyVariables()).ToArrayAsync());
+            Assert.Equal(FeatureAiteratorUseCase.Expect_DiffApplyVariables, actual);
+
+            actual.Clear();
+            await Assert.ThrowsAsync<NotImplementedException>(async () => await ((IAsyncEnumerable<string>)instance.DiffApplyArray()).ToArrayAsync());
+            Assert.Equal(FeatureAiteratorUseCase.Expect_DiffApplyArray, actual);
+        }
+    }
+}
---
+++ test/TestAssemblies/BasicUsage/FeatureAiteratorUseCase.cs
@@ -0,0 +1,227 @@
+﻿using BasicUsage.Attributes;
+using Rougamo;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BasicUsage
+{
+    public class FeatureAiteratorUseCase : IRecording
+    {
+        public List<string> Recording { get; } = new List<string>();
+
+        #region OnEntry
+        public static string[] Expect_OnEntry = new[] { "OnEntry-1" };
+
+        [Feature(1, Features = Feature.OnEntry)]
+        public async IAsyncEnumerable<string> OnEntry(string value)
+        {
+            yield return nameof(FeatureIteratorUseCase);
+            yield return nameof(DiffApplyArray);
+            await Task.Yield();
+            yield return value;
+        }
+
+        [Feature(1, Features = Feature.OnEntry)]
+        public async IAsyncEnumerable<string> OnEntry_RewriteArgs(Guid guid)
+        {
+            yield return nameof(FeatureIteratorUseCase);
+            yield return nameof(DiffApplyArray);
+            await Task.Yield();
+            yield return guid.ToString();
+        }
+
+        [Feature(1, Features = Feature.OnEntry)]
+        public async IAsyncEnumerable<string> OnEntry_Exception()
+        {
+            yield return nameof(FeatureIteratorUseCase);
+            yield return nameof(DiffApplyArray);
+            await Task.Yield();
+            throw new NotImplementedException();
+        }
+        #endregion OnEntry
+
+        #region OnException
+        public static string[] Expect_OnException = new[] { "OnException-1" };
+
+        [Feature(1, Features = Feature.OnException)]
+        public async IAsyncEnumerable<string> OnException(string value)
+        {
+            yield return nameof(FeatureIteratorUseCase);
+            yield return nameof(DiffApplyArray);
+            await Task.Yield();
+            yield return value;
+        }
+
+        [Feature(1, Features = Feature.OnException)]
+        public async IAsyncEnumerable<string> OnException_RewriteArgs(Guid guid)
+        {
+            yield return nameof(FeatureIteratorUseCase);
+            yield return nameof(DiffApplyArray);
+            await Task.Yield();
+            yield return guid.ToString();
+        }
+
+        [Feature(1, Features = Feature.OnException)]
+        public async IAsyncEnumerable<string> OnException_Exception()
+        {
+            yield return nameof(FeatureIteratorUseCase);
+            yield return nameof(DiffApplyArray);
+            await Task.Yield();
+            throw new NotImplementedException();
+        }
+        #endregion OnException
+
+        #region OnSuccess
+        public static string[] Expect_OnSuccess = new[] { "OnSuccess-1" };
+
+        [Feature(1, Features = Feature.OnSuccess)]
+        public async IAsyncEnumerable<string> OnSuccess(string value)
+        {
+            yield return nameof(FeatureIteratorUseCase);
+            yield return nameof(DiffApplyArray);
+            await Task.Yield();
+            yield return value;
+        }
+
+        [Feature(1, Features = Feature.OnSuccess)]
+        public async IAsyncEnumerable<string> OnSuccess_RewriteArgs(Guid guid)
+        {
+            yield return nameof(FeatureIteratorUseCase);
+            yield return nameof(DiffApplyArray);
+            await Task.Yield();
+            yield return guid.ToString();
+        }
+
+        [Feature(1, Features = Feature.OnSuccess)]
+        public async IAsyncEnumerable<string> OnSuccess_Exception()
+        {
+            yield return nameof(FeatureIteratorUseCase);
+            yield return nameof(DiffApplyArray);
+            await Task.Yield();
+            throw new NotImplementedException();
+        }
+        #endregion OnSuccess
+
+        #region OnExit
+        public static string[] Expect_OnExit = new[] { "OnExit-1" };
+
+        [Feature(1, Features = Feature.OnExit)]
+        public async IAsyncEnumerable<string> OnExit(string value)
+        {
+            yield return nameof(FeatureIteratorUseCase);
+            yield return nameof(DiffApplyArray);
+            await Task.Yield();
+            yield return value;
+        }
+
+        [Feature(1, Features = Feature.OnExit)]
+        public async IAsyncEnumerable<string> OnExit_RewriteArgs(Guid guid)
+        {
+            yield return nameof(FeatureIteratorUseCase);
+            yield return nameof(DiffApplyArray);
+            await Task.Yield();
+            yield return guid.ToString();
+        }
+
+        [Feature(1, Features = Feature.OnExit)]
+        public async IAsyncEnumerable<string> OnExit_Exception()
+        {
+            yield return nameof(FeatureIteratorUseCase);
+            yield return nameof(DiffApplyArray);
+            await Task.Yield();
+            throw new NotImplementedException();
+        }
+        #endregion OnExit
+
+        #region RewriteArgs
+        public static string[] Expect_RewriteArgs = new[] { "OnEntry-1" };
+
+        [Feature(1, Features = Feature.RewriteArgs)]
+        public async IAsyncEnumerable<string> RewriteArgs(string value)
+        {
+            yield return nameof(FeatureIteratorUseCase);
+            yield return nameof(DiffApplyArray);
+            await Task.Yield();
+            yield return value;
+        }
+
+        [Feature(1, Features = Feature.RewriteArgs)]
+        public async IAsyncEnumerable<string> RewriteArgs_RewriteArgs(Guid guid)
+        {
+            yield return nameof(FeatureIteratorUseCase);
+            yield return nameof(DiffApplyArray);
+            await Task.Yield();
+            yield return guid.ToString();
+        }
+
+        [Feature(1, Features = Feature.RewriteArgs)]
+        public async IAsyncEnumerable<string> RewriteArgs_Exception()
+        {
+            yield return nameof(FeatureIteratorUseCase);
+            yield return nameof(DiffApplyArray);
+            await Task.Yield();
+            throw new NotImplementedException();
+        }
+        #endregion RewriteArgs
+
+        #region OnEntry and OnExit
+        public static string[] Expect_OnEntryExit = new[] { "OnEntry-1", "OnExit-1" };
+
+        [Feature(1, Features = Feature.OnEntry | Feature.OnExit)]
+        public async IAsyncEnumerable<string> OnEntryExit(string value)
+        {
+            yield return nameof(FeatureIteratorUseCase);
+            yield return nameof(DiffApplyArray);
+            await Task.Yield();
+            yield return value;
+        }
+
+        [Feature(1, Features = Feature.OnEntry | Feature.OnExit)]
+        public async IAsyncEnumerable<string> OnEntryExit_RewriteArgs(Guid guid)
+        {
+            yield return nameof(FeatureIteratorUseCase);
+            yield return nameof(DiffApplyArray);
+            await Task.Yield();
+            yield return guid.ToString();
+        }
+
+        [Feature(1, Features = Feature.OnEntry | Feature.OnExit)]
+        public async IAsyncEnumerable<string> OnEntryExit_Exception()
+        {
+            yield return nameof(FeatureIteratorUseCase);
+            yield return nameof(DiffApplyArray);
+            await Task.Yield();
+            throw new NotImplementedException();
+        }
+        #endregion OnEntry and OnExit
+
+        #region Different Feature Apply
+        public static string[] Expect_DiffApplyVariables = new[] { "OnEntry-1", "OnEntry-3", "OnException-3", "OnException-2" };
+        [Feature(1, Features = Feature.OnEntry)]
+        [Feature(2, Features = Feature.OnException)]
+        [Feature(3, Features = Feature.OnEntry | Feature.OnException)]
+        public async IAsyncEnumerable<string> DiffApplyVariables()
+        {
+            yield return nameof(FeatureIteratorUseCase);
+            yield return nameof(DiffApplyArray);
+            await Task.Yield();
+            throw new NotImplementedException();
+        }
+
+        public static string[] Expect_DiffApplyArray = new[] { "OnEntry-1", "OnEntry-3", "OnEntry-4", "OnEntry-5", "OnException-4", "OnException-2" };
+        [Feature(1, Features = Feature.OnEntry)]
+        [Feature(2, Features = Feature.OnException)]
+        [Feature(3, Features = Feature.OnEntry)]
+        [Feature(4, Features = Feature.OnEntry | Feature.OnException)]
+        [Feature(5, Features = Feature.OnEntry)]
+        public async IAsyncEnumerable<string> DiffApplyArray()
+        {
+            yield return nameof(FeatureIteratorUseCase);
+            yield return nameof(DiffApplyArray);
+            await Task.Yield();
+            throw new NotImplementedException();
+        }
+        #endregion Different Feature Apply
+    }
+}
