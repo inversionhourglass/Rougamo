b020fea9466bfe01f8fc22f6ded778192f3caa6c
ihourglass <inversionhourglass@outlook.com>
2024-04-29 18:00:09 +0800

    #60. Add test case.

---
+++ test/TestAssemblies/Issues/Attributes/_60_Attribute.cs
@@ -0,0 +1,40 @@
+﻿using Rougamo;
+using Rougamo.Context;
+using System.Collections.Generic;
+
+namespace Issues.Attributes
+{
+    public class _60_Attribute : MoAttribute
+    {
+        public static readonly string[] SUCCEED = [nameof(OnEntry), nameof(OnSuccess), nameof(OnExit)];
+        public static readonly string[] FAILED = [nameof(OnEntry), nameof(OnException), nameof(OnExit)];
+
+        private readonly List<string> _executed = [];
+
+        public override void OnEntry(MethodContext context)
+        {
+            if (context.Arguments.Length == 1 && context.Arguments[0] is List<string>)
+            {
+                context.Arguments[0] = _executed;
+                context.RewriteArguments = true;
+
+                _executed.Add(nameof(OnEntry));
+            }
+        }
+
+        public override void OnSuccess(MethodContext context)
+        {
+            _executed.Add(nameof(OnSuccess));
+        }
+
+        public override void OnException(MethodContext context)
+        {
+            _executed.Add(nameof(OnException));
+        }
+
+        public override void OnExit(MethodContext context)
+        {
+            _executed.Add(nameof(OnExit));
+        }
+    }
+}
---
+++ test/TestAssemblies/Issues/Issue60.cs
@@ -0,0 +1,57 @@
+﻿using Issues.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Issues
+{
+    public class Issue60
+    {
+        [_60_]
+        public async CompletedTask Async(List<string> items)
+        {
+            await default(CompletedTask);
+        }
+
+        [_60_]
+        public async CompletedTask AsyncException(List<string> items)
+        {
+            await default(CompletedTask);
+            throw new Exception();
+        }
+
+        public class MethodBuilder
+        {
+            public CompletedTask Task => default;
+
+            public static MethodBuilder Create() => default;
+
+            public void Start<TStateMachine>(ref TStateMachine stateMachine) where TStateMachine : IAsyncStateMachine => stateMachine.MoveNext();
+
+            public void SetStateMachine(IAsyncStateMachine stateMachine) { }
+
+            public void SetResult() { }
+
+            public void SetException(Exception exception) { }
+
+            public void AwaitOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine) where TAwaiter : INotifyCompletion where TStateMachine : IAsyncStateMachine { }
+
+            public void AwaitUnsafeOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine) where TAwaiter : ICriticalNotifyCompletion where TStateMachine : IAsyncStateMachine { }
+        }
+
+        [AsyncMethodBuilder(typeof(MethodBuilder))]
+        public struct CompletedTask
+        {
+            public CompletedTaskAwaiter GetAwaiter() => default;
+        }
+
+        public class CompletedTaskAwaiter : INotifyCompletion
+        {
+            public bool IsCompleted => true;
+
+            public void GetResult() { }
+
+            public void OnCompleted(Action continuation) { }
+        }
+    }
+}
