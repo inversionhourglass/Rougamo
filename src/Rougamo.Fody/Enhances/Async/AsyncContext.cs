23fb6f159712b93df825a4fbac62c13b14c68e80
ihourglass <inversionhourglass@outlook.com>
2024-07-13 19:24:53 +0800

    Add AsyncContext to save the global anchors and useful variables.

---
+++ src/Rougamo.Fody/Enhances/Async/AsyncContext.cs
@@ -0,0 +1,31 @@
+﻿using Mono.Cecil.Cil;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rougamo.Fody.Enhances.Async
+{
+    internal class AsyncContext(RouMethod rouMethod)
+    {
+        public bool OnExceptionAllInSync { get; } = rouMethod.Mos.All(x => x.ForceSync.Contains(ForceSync.OnException));
+
+        public bool OnExitAllInSync { get; } = rouMethod.Mos.All(x => x.ForceSync.Contains(ForceSync.OnExit));
+
+        public int State { get; set; } = 0;
+
+        public bool AwaitFirst { get; set; } = false;
+
+        public List<Instruction> AnchorSwitches { get; } = [];
+
+        public Instruction AnchorProxyCallCase { get; } = Instruction.Create(OpCodes.Nop);
+
+        public Instruction AnchorOnExceptionAsync { get; } = Instruction.Create(OpCodes.Nop);
+
+        public Instruction AnchorOnExitAsync { get; } = Instruction.Create(OpCodes.Nop);
+
+        public Instruction AnchorStateReady { get; private set; } = Instruction.Create(OpCodes.Nop);
+
+        public Instruction AnchorSetResult { get; } = Instruction.Create(OpCodes.Nop);
+
+        public Instruction GetNextStateReadyAnchor() => AnchorStateReady = Instruction.Create(OpCodes.Nop);
+    }
+}
