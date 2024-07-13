6558caeb2c8b7d945f11141f0fb32faf4cffc322
ihourglass <inversionhourglass@outlook.com>
2024-07-13 15:53:58 +0800

    Now MethodSimulation can infer the generic type from the arguments.

--- src/Rougamo.Fody/Simulations/MethodSimulation.cs
+++ src/Rougamo.Fody/Simulations/MethodSimulation.cs
@@ -1,6 +1,7 @@
 ﻿using Mono.Cecil;
 using Mono.Cecil.Cil;
 using Rougamo.Fody.Simulations.PlainValues;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,11 +9,14 @@ namespace Rougamo.Fody.Simulations
 {
     internal class MethodSimulation : Simulation
     {
+        private readonly int[]? _genericParaIndexes;
+
         public MethodSimulation(TypeSimulation declaringType, MethodDefinition methodDef) : base(declaringType.Module)
         {
             DeclaringType = declaringType;
             Def = methodDef;
-            Ref = methodDef.WithGenericDeclaringType(declaringType);
+            Ref = methodDef.WithGenericDeclaringType(declaringType).ImportInto(Module);
+            _genericParaIndexes = GetGenericParameterIndexes();
         }
 
         public MethodSimulation(TypeSimulation declaringType, MethodReference methodRef) : base(declaringType.Module)
@@ -20,6 +24,7 @@ namespace Rougamo.Fody.Simulations
             DeclaringType = declaringType;
             Def = methodRef.Resolve();
             Ref = methodRef;
+            _genericParaIndexes = GetGenericParameterIndexes();
         }
 
         public TypeSimulation DeclaringType { get; }
@@ -30,21 +35,27 @@ namespace Rougamo.Fody.Simulations
 
         public VariableDefinition? TempThis { get; set; }
 
-        public virtual IList<Instruction> DupCall(TypeSimulation[]? generics, params IParameterSimulation?[] arguments)
+        public virtual IList<Instruction> DupCall(params IParameterSimulation?[] arguments)
         {
-            return Call(true, generics, arguments);
+            return Call(true, null, arguments);
         }
 
-        public virtual IList<Instruction> Call(TypeSimulation[]? generics, params IParameterSimulation?[] arguments)
+        public virtual IList<Instruction> Call(params IParameterSimulation?[] arguments)
         {
-            return Call(false, generics, arguments);
+            return Call(false, null, arguments);
         }
 
-        private IList<Instruction> Call(bool dupCalling, TypeSimulation[]? generics, params IParameterSimulation?[] arguments)
+        public IList<Instruction> Call(bool dupCalling, TypeSimulation[]? generics, params IParameterSimulation?[] arguments)
         {
             if (Def.Parameters.Count != arguments.Length) throw new RougamoException($"Parameters count not match of method {Def}, need {Def.Parameters.Count} gave {arguments.Length}");
 
             var instructions = new List<Instruction>();
+
+            if (generics == null)
+            {
+                if (_genericParaIndexes == null) throw new RougamoException($"[{Def}] Not all method generic parameters are inferred from parameters, you have to specify all the generic parameters manually.");
+                generics = _genericParaIndexes.Select(x => arguments[x]?.Type ?? throw new RougamoException($"[{Def}] Cannot infer the generic type from a null value at parameter index {x}")).ToArray();
+            }
 
             var methodRef = generics == null ? Ref : Ref.WithGenerics(generics.Select(x => x.Ref).ToArray());
 
@@ -57,7 +68,7 @@ namespace Rougamo.Fody.Simulations
                 }
             }
 
-            if (!Def.IsConstructor)
+            if (!Def.IsConstructor && !Def.IsStatic)
             {
                 if (dupCalling)
                 {
@@ -95,6 +106,28 @@ namespace Rougamo.Fody.Simulations
         public VariableSimulation<T> CreateVariable<T>(TypeReference variableTypeRef) where T : TypeSimulation
         {
             return Def.Body.CreateVariable(variableTypeRef).Simulate<T>(this);
+        }
+
+        private int[]? GetGenericParameterIndexes()
+        {
+            if (!Def.HasGenericParameters) return [];
+
+            var indexes = Enumerable.Repeat(-1, Def.GenericParameters.Count).ToArray();
+            var index = 0;
+            var map = new Dictionary<GenericParameter, int>();
+            foreach (var gp in Def.GenericParameters)
+            {
+                map.Add(gp, index++);
+            }
+            for (var i = 0; i < Def.Parameters.Count; i++)
+            {
+                if (Def.Parameters[i].ParameterType is GenericParameter gp && map.TryGetValue(gp, out var idx))
+                {
+                    indexes[idx] = i;
+                }
+            }
+
+            return indexes.Contains(-1) ? null : indexes;
         }
 
         public static implicit operator MethodReference(MethodSimulation value) => value.Ref;
