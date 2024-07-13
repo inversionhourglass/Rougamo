0bbaaa88b06baa8a696d234bedc29f90a55fd2d9
ihourglass <inversionhourglass@outlook.com>
2024-07-13 15:55:00 +0800

    Add a new plain value simulation type Int32Value.

---
+++ src/Rougamo.Fody/Simulations/PlainValues/Int32Value.cs
@@ -0,0 +1,12 @@
+﻿using Mono.Cecil.Cil;
+using System.Collections.Generic;
+
+namespace Rougamo.Fody.Simulations.PlainValues
+{
+    internal class Int32Value(int value) : PlainValueSimulation(null!), IParameterSimulation
+    {
+        public override TypeSimulation Type => GlobalSimulations.Int32;
+
+        public override IList<Instruction> Load() => [Instruction.Create(OpCodes.Ldc_I4, value)];
+    }
+}
