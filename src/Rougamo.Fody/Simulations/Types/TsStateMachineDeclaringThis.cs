58e980bd71cdb14fdea21ba9ad803cb6fbc079c0
ihourglass <inversionhourglass@outlook.com>
2024-07-13 19:22:03 +0800

    Rename the class from TsStateMachineDeclaringThis to TsWeaveingTarget.

--- src/Rougamo.Fody/Simulations/Types/TsStateMachineDeclaringThis.cs
+++
@@ -1,8 +0,0 @@
-﻿using Mono.Cecil;
-
-namespace Rougamo.Fody.Simulations.Types
-{
-    internal class TsStateMachineDeclaringThis(TypeReference typeRef, IHost? host, ModuleDefinition moduleDef) : TypeSimulation(typeRef, host, moduleDef)
-    {
-    }
-}
---
+++ src/Rougamo.Fody/Simulations/Types/TsWeaveingTarget.cs
@@ -0,0 +1,8 @@
+﻿using Mono.Cecil;
+
+namespace Rougamo.Fody.Simulations.Types
+{
+    internal class TsWeaveingTarget(TypeReference typeRef, IHost? host, ModuleDefinition moduleDef) : TypeSimulation(typeRef, host, moduleDef)
+    {
+    }
+}
