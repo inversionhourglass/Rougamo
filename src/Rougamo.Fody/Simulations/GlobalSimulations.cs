6c2f4d037bcfce3599d4c39fdc2b6e76b468cf82
ihourglass <inversionhourglass@outlook.com>
2024-07-13 15:52:21 +0800

    Enrich the members of GlobalSimulations.

--- src/Rougamo.Fody/Simulations/GlobalSimulations.cs
+++ src/Rougamo.Fody/Simulations/GlobalSimulations.cs
@@ -3,7 +3,8 @@
     internal static class GlobalSimulations
     {
         public static TypeSimulation Object = GlobalRefs.TrObject.Simulate(null!);
-        public static TypeSimulation Bool = GlobalRefs.TrInt.Simulate(null!);
+        public static TypeSimulation Bool = GlobalRefs.TrBool.Simulate(null!);
+        public static TypeSimulation Int32 = GlobalRefs.TrInt.Simulate(null!);
         public static TypeSimulation Type = GlobalRefs.TrType.Simulate(null!);
         public static TypeSimulation MethodBase = GlobalRefs.TrMethodBase.Simulate(null!);
     }
