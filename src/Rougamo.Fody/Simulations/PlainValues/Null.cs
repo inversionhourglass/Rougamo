177106b57a5154f73c9ac6728b7691c1fa321ebe
ihourglass <inversionhourglass@outlook.com>
2024-07-17 20:44:21 +0800

    Now we can set the type of null value.

--- src/Rougamo.Fody/Simulations/PlainValues/Null.cs
+++ src/Rougamo.Fody/Simulations/PlainValues/Null.cs
@@ -4,14 +4,19 @@ using System.Collections.Generic;
 
 namespace Rougamo.Fody.Simulations.PlainValues
 {
-    internal class Null() : PlainValueSimulation(null!)
+    internal class Null(TypeSimulation? type = null) : PlainValueSimulation(null!)
     {
         private static readonly TypeSimulation _Type = GlobalRefs.TrObject.Simulate(null!);
 
-        public override TypeSimulation Type => _Type;
+        public override TypeSimulation Type => type ?? _Type;
 
         public override IList<Instruction> Load() => [Instruction.Create(OpCodes.Ldnull)];
 
         public override IList<Instruction> Cast(TypeReference to) => [];
+    }
+
+    internal static class NullExtensions
+    {
+        public static Null Null(this TypeSimulation type) => new(type);
     }
 }
