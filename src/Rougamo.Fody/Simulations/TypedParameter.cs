cc515b9a4866990638c802a8ae3de6b7f8d0c79f
ihourglass <inversionhourglass@outlook.com>
2024-07-17 20:45:15 +0800

    Add a type TypedParameter

---
+++ src/Rougamo.Fody/Simulations/TypedParameter.cs
@@ -0,0 +1,31 @@
+﻿using Mono.Cecil;
+using Mono.Cecil.Cil;
+using System.Collections.Generic;
+
+namespace Rougamo.Fody.Simulations
+{
+    internal class TypedParameter(TypeSimulation type, IParameterSimulation parameter) : IParameterSimulation
+    {
+        public TypeSimulation Type => type;
+
+        public OpCode TrueToken => parameter.TrueToken;
+
+        public OpCode FalseToken => parameter.FalseToken;
+
+        public IList<Instruction> Cast(TypeReference to) => parameter.Cast(to);
+
+        public IList<Instruction> Load() => parameter.Load();
+
+        public IList<Instruction> LoadAddress(MethodSimulation method) => parameter.LoadAddress(method);
+
+        public IList<Instruction> PrepareLoadAddress(MethodSimulation method) => parameter.PrepareLoadAddress(method);
+    }
+
+    internal static class TypedParameterExtensions
+    {
+        public static TypedParameter Typed(this IParameterSimulation parameter, TypeSimulation type)
+        {
+            return new(type, parameter);
+        }
+    }
+}
