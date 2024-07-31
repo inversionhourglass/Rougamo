46d6ae6c768b189405a1e15e5b4b8ee937f3c209
ihourglass <inversionhourglass@outlook.com>
2024-07-31 18:45:55 +0800

    Add a new simulation type TsStateMachine, and extract the general part from TsAsyncStateMachine into TsStateMachine.

--- src/Rougamo.Fody/Simulations/Types/TsAsyncStateMachine.cs
+++ src/Rougamo.Fody/Simulations/Types/TsAsyncStateMachine.cs
@@ -5,22 +5,10 @@ using System.Linq;
 namespace Rougamo.Fody.Simulations.Types
 {
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-    internal class TsAsyncStateMachine(TypeReference typeRef, IHost? host, ModuleWeaver moduleWeaver) : TypeSimulation(typeRef, host, moduleWeaver)
+    internal class TsAsyncStateMachine(TypeReference typeRef, IHost? host, ModuleWeaver moduleWeaver) : TsStateMachine(typeRef, host, moduleWeaver)
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     {
-        public FieldSimulation? F_MoArray { get; private set; }
-
-        public FieldSimulation<TsMo>[]? F_Mos { get; private set; }
-
-        public FieldSimulation<TsMethodContext> F_MethodContext { get; private set; }
-
-        public FieldSimulation F_State { get; private set; }
-
         public FieldSimulation<TsAsyncBuilder> F_Builder { get; private set; }
-
-        public FieldSimulation[] F_Parameters { get; private set; }
-
-        public FieldSimulation<TsWeavingTarget>? F_DeclaringThis { get; private set; }
 
         public FieldSimulation<TsAwaiter> F_Awaiter { get; private set; }
 
@@ -28,11 +16,9 @@ namespace Rougamo.Fody.Simulations.Types
 
         public FieldSimulation? F_Result { get; private set; }
 
-        public MethodSimulation M_MoveNext => MethodSimulate(Constants.METHOD_MoveNext, false);
-
         public TsAsyncStateMachine SetFields(AsyncFields fields)
         {
-            F_MoArray = fields.MoArray?.Resolve().Simulate(this);
+            F_MoArray = fields.MoArray?.Resolve().Simulate<TsArray<TsMo>>(this);
             F_Mos = fields.Mos.Select(x => x.Resolve().Simulate<TsMo>(this)).ToArray();
             F_MethodContext = fields.MethodContext.Resolve().Simulate<TsMethodContext>(this);
             F_State = fields.State.Resolve().Simulate(this);
---
+++ src/Rougamo.Fody/Simulations/Types/TsStateMachine.cs
@@ -0,0 +1,23 @@
+﻿using Mono.Cecil;
+
+namespace Rougamo.Fody.Simulations.Types
+{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    internal abstract class TsStateMachine(TypeReference typeRef, IHost? host, ModuleWeaver moduleWeaver) : TypeSimulation(typeRef, host, moduleWeaver)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    {
+        public FieldSimulation<TsArray<TsMo>>? F_MoArray { get; protected set; }
+
+        public FieldSimulation<TsMo>[]? F_Mos { get; protected set; }
+
+        public FieldSimulation<TsMethodContext> F_MethodContext { get; protected set; }
+
+        public FieldSimulation F_State { get; protected set; }
+
+        public FieldSimulation[] F_Parameters { get; protected set; }
+
+        public FieldSimulation<TsWeavingTarget>? F_DeclaringThis { get; protected set; }
+
+        public MethodSimulation M_MoveNext => MethodSimulate(Constants.METHOD_MoveNext, false);
+    }
+}
