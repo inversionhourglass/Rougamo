a682e6715fbbb8301c315d410c092f52ff4cea1e
ihourglass <inversionhourglass@outlook.com>
2024-08-04 16:50:45 +0800

    Delete useless Constants.

--- src/Rougamo.Fody/Constants.cs
+++ src/Rougamo.Fody/Constants.cs
@@ -3,9 +3,7 @@
     internal static class Constants
     {
         public const string TYPE_IMo = "Rougamo.IMo";
-        public const string TYPE_MoAttribute = "Rougamo.MoAttribute";
         public const string TYPE_RougamoAttribute = "Rougamo.RougamoAttribute";
-        public const string TYPE_RougamoAttribute_1 = "Rougamo.RougamoAttribute`1";
         public const string TYPE_MoProxyAttribute = "Rougamo.MoProxyAttribute";
         public const string TYPE_IgnoreMoAttribute = "Rougamo.IgnoreMoAttribute";
         public const string TYPE_MoRepulsion = "Rougamo.MoRepulsion";
@@ -14,10 +12,7 @@
         public const string TYPE_Omit = "Rougamo.Context.Omit";
         public const string TYPE_ForceSync = "Rougamo.ForceSync";
         public const string TYPE_IRougamo = "Rougamo.IRougamo";
-        public const string TYPE_IRougamo_1 = "Rougamo.IRougamo`1";
-        public const string TYPE_IRougamo_2 = "Rougamo.IRougamo`2";
         public const string TYPE_IRepulsionsRougamo = "Rougamo.IRepulsionsRougamo";
-        public const string TYPE_MethodContext = "Rougamo.Context.MethodContext";
 
         public const string TYPE_Void = "System.Void";
         public const string TYPE_String = "System.String";
@@ -36,7 +31,6 @@
         public const string TYPE_Task = "System.Threading.Tasks.Task";
         public const string TYPE_ValueTask = "System.Threading.Tasks.ValueTask";
         public const string TYPE_MulticastDelegate = "System.MulticastDelegate";
-        public const string TYPE_ObsoleteAttribute = "System.ObsoleteAttribute";
         public const string TYPE_IEnumerable = "System.Collections.Generic.IEnumerable";
         public const string TYPE_IAsyncEnumerable = "System.Collections.Generic.IAsyncEnumerable";
         public const string TYPE_CompilerGeneratedAttribute = "System.CompilerGeneratedAttribute";
@@ -47,12 +41,6 @@
         public const string TYPE_IteratorStateMachineAttribute = "System.Runtime.CompilerServices.IteratorStateMachineAttribute";
         public const string TYPE_AsyncIteratorStateMachineAttribute = "System.Runtime.CompilerServices.AsyncIteratorStateMachineAttribute";
         public const string TYPE_AsyncMethodBuilderAttribute = "System.Runtime.CompilerServices.AsyncMethodBuilderAttribute";
-        public const string TYPE_AsyncMethodBuilder = "System.Runtime.CompilerServices.AsyncMethodBuilder";
-        public const string TYPE_AsyncTaskMethodBuilder = "System.Runtime.CompilerServices.AsyncTaskMethodBuilder"; // async Task
-        public const string TYPE_AsyncValueTaskMethodBuilder = "System.Runtime.CompilerServices.AsyncValueTaskMethodBuilder"; // async ValueTask
-        public const string TYPE_AsyncVoidMethodBuilder = "System.Runtime.CompilerServices.AsyncVoidMethodBuilder"; // async void
-        public const string TYPE_ManualResetValueTaskSourceCore = "System.Threading.Tasks.Sources.ManualResetValueTaskSourceCore"; // async IAsyncEnumerable
-        public const string TYPE_ManualResetValueTaskSourceCore_Bool = "System.Threading.Tasks.Sources.ManualResetValueTaskSourceCore`1<System.Boolean>";
 
         public const string TYPE_ARRAY_Type = "System.Type[]";
 
@@ -66,7 +54,6 @@
         public const string PROP_Repulsions = "Repulsions";
         public const string PROP_MoTypes = "MoTypes";
         public const string PROP_Exception = "Exception";
-        public const string PROP_HasException = "HasException";
         public const string PROP_ReturnValue = "ReturnValue";
         public const string PROP_ExceptionHandled = "ExceptionHandled";
         public const string PROP_ReturnValueReplaced = "ReturnValueReplaced";
@@ -80,9 +67,6 @@
 
         public const string FIELD_Format = "<{0}>k__BackingField";
         public const string FIELD_Repulsions = "<Repulsions>k__BackingField";
-        // Private fields cannot be accessed externally even using IL
-        //public const string FIELD_Exception = "<Exception>k__BackingField";
-        //public const string FIELD_ReturnValue = "<ReturnValue>k__BackingField";
         public const string FIELD_RougamoMo_Prefix = ">_<rougamo_mo_";
         public const string FIELD_RougamoMos = ">_<rougamo_mos";
         public const string FIELD_RougamoContext = ">_<rougamo_context";
@@ -108,7 +92,6 @@
         public const string METHOD_OnExceptionAsync = "OnExceptionAsync";
         public const string METHOD_OnExitAsync = "OnExitAsync";
         public const string METHOD_Ctor = ".ctor";
-        public const string METHOD_IsMatch = "IsMatch";
         public const string METHOD_Create = "Create";
         public const string METHOD_MoveNext = "MoveNext";
         public const string METHOD_SetStateMachine = "SetStateMachine";
