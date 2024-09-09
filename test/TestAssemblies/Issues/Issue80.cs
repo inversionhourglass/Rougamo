cdf258e2e4cb3103e96f7c09f5fc73a3cbbe6daa
ihourglass <inversionhourglass@outlook.com>
2024-09-09 17:33:29 +0800

    Add a libaray import method test case (#80)

--- test/TestAssemblies/Issues/Issue80.cs
+++ test/TestAssemblies/Issues/Issue80.cs
@@ -5,11 +5,16 @@ using System.Runtime.InteropServices;
 namespace Issues
 {
     [_80_]
-    public class Issue80
+    public partial class Issue80
     {
         public void M() { }
 
         [DllImport("user32.dll")]
         private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+#if NET7_0_OR_GREATER
+        [LibraryImport("libSystem.dylib")]
+        private static partial int getpid();
+#endif
     }
 }
