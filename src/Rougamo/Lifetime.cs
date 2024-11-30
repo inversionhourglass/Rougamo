ee50cdb63ebbf3227ad0a4fe2af6c92ddab2b87a
ihourglass <inversionhourglass@outlook.com>
2024-12-01 01:30:31 +0800

    Add LifetimeAttribute.

---
+++ src/Rougamo/Lifetime.cs
@@ -0,0 +1,29 @@
+﻿#if FODY
+namespace Rougamo.Fody
+#else
+namespace Rougamo
+#endif
+{
+    /// <summary>
+    /// The lifetime of Mo.
+    /// </summary>
+#if FODY
+    internal enum Lifetime
+#else
+    public enum Lifetime
+#endif
+    {
+        /// <summary>
+        /// Singleton
+        /// </summary>
+        Singleton,
+        /// <summary>
+        /// Pooled
+        /// </summary>
+        Pooled,
+        /// <summary>
+        /// Transient
+        /// </summary>
+        Transient
+    }
+}
---
+++ src/Rougamo/Metadatas/LifetimeAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Rougamo.Metadatas
+{
+    /// <summary>
+    /// Define the lifetime of the type.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Struct | AttributeTargets.Class)]
+    public sealed class LifetimeAttribute(Lifetime lifetime) : Attribute
+    {
+        /// <summary>
+        /// The lifetime of the type.
+        /// </summary>
+        public Lifetime Lifetime { get; } = lifetime;
+    }
+}
