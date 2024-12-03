1b55be472a5de9c9af82e871ff8cfd4e65a14202
ihourglass <inversionhourglass@outlook.com>
2024-12-03 21:34:17 +0800

    Add IAsyncMo and ISyncMo that has default implementation of methods.

---
+++ src/Rougamo/IAsyncMo.cs
@@ -0,0 +1,36 @@
+﻿#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER
+using Rougamo.Context;
+
+namespace Rougamo
+{
+    /// <summary>
+    /// For the struct Mo, provide a default implementation of the sync aspect method.
+    /// </summary>
+    public interface IAsyncMo : IMo
+    {
+        /// <inheritdoc/>
+        void IMo.OnEntry(MethodContext context)
+        {
+            OnEntryAsync(context).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        /// <inheritdoc/>
+        void IMo.OnSuccess(MethodContext context)
+        {
+            OnSuccessAsync(context).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        /// <inheritdoc/>
+        void IMo.OnException(MethodContext context)
+        {
+            OnExceptionAsync(context).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        /// <inheritdoc/>
+        void IMo.OnExit(MethodContext context)
+        {
+            OnExitAsync(context).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+    }
+}
+#endif
---
+++ src/Rougamo/ISyncMo.cs
@@ -0,0 +1,45 @@
+﻿#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER
+using Rougamo.Context;
+using System.Threading.Tasks;
+
+namespace Rougamo
+{
+    /// <summary>
+    /// For the struct Mo, provide a default implementation of the async aspect method.
+    /// </summary>
+    public interface ISyncMo : IMo
+    {
+        /// <inheritdoc />
+        ValueTask IMo.OnEntryAsync(MethodContext context)
+        {
+            OnEntry(context);
+
+            return default;
+        }
+
+        /// <inheritdoc />
+        ValueTask IMo.OnSuccessAsync(MethodContext context)
+        {
+            OnSuccess(context);
+
+            return default;
+        }
+
+        /// <inheritdoc />
+        ValueTask IMo.OnExceptionAsync(MethodContext context)
+        {
+            OnException(context);
+
+            return default;
+        }
+
+        /// <inheritdoc />
+        ValueTask IMo.OnExitAsync(MethodContext context)
+        {
+            OnExit(context);
+
+            return default;
+        }
+    }
+}
+#endif
